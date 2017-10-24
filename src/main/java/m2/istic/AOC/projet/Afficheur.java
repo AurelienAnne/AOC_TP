f3d32c02579494a310d5bb34109fa77392d43e98
pierre <pierre@fedora.pierre>
2017-10-24 14:39:09 +0200

    Implemente l'Afficheur.

--- src/main/java/m2/istic/AOC/projet/Afficheur.java
+++ src/main/java/m2/istic/AOC/projet/Afficheur.java
@@ -3,8 +3,7 @@ package m2.istic.AOC.projet;
 public class Afficheur implements ObservateurGenerateur{
 
 	public void update(Generateur subject) {
-		// TODO Auto-generated method stub
-		
+		System.out.println(subject.getValue());
 	}
 
 
