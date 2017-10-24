851b5aa23652b6d105668820cb3ceabfba26e461
pierre <pierre@fedora.pierre>
2017-10-24 14:56:12 +0200

    Corrige les interfaces pour binder correctement Generateur.

--- src/main/java/m2/istic/AOC/projet/Canal.java
+++ src/main/java/m2/istic/AOC/projet/Canal.java
@@ -1,15 +1,20 @@
 package m2.istic.AOC.projet;
 
-public class Canal implements Generateur, ObservateurGenerateur{
+public class Canal implements Generateur, ObservateurGenerateur {
 
-	public void attach(Observer<?> o) {
+	public void attach(Observer<Generateur> o) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	public void detach(Observer<?> o) {
+	public void detach(Observer<Generateur> o) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public void notifyObservers() {
+
 	}
 
 	public void update(Generateur subject) {
@@ -22,4 +27,6 @@ public class Canal implements Generateur, ObservateurGenerateur{
 		return null;
 	}
 
+	@Override
+	public void generateValue() {}
 }
--- src/main/java/m2/istic/AOC/projet/Generateur.java
+++ src/main/java/m2/istic/AOC/projet/Generateur.java
@@ -3,4 +3,5 @@ package m2.istic.AOC.projet;
 public interface Generateur extends Subject<Generateur> {
 
 	public Integer getValue();
+	public void generateValue();
 }
