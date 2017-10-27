7d5cd1968a8eb986aa62194311672b5620c87f7f
pierre <pierre@fedora.pierre>
2017-10-27 17:45:47 +0200

    Refactor 4 afficheurs into a for loop.

--- src/main/java/m2/istic/AOC/projet/App.java
+++ src/main/java/m2/istic/AOC/projet/App.java
@@ -16,31 +16,18 @@ public class App
 {
     public static void main( String[] args ) {
 
-        Observer<GenerateurAsync> afficheur = new Afficheur();
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(10);
         AlgoDiffusion algo = new SequentialDiffusion();
         Generateur generateur = new GenerateurImpl(0, algo);
+        for (int i = 0; i < 4; i++) {
+            int delay = (int) (100 * Math.pow(3, i));
+            Observer<GenerateurAsync> afficheur = new Afficheur();
+            Canal canal = new Canal(generateur, afficheur, scheduler, delay);
+            generateur.attach(canal);
+            canal.attach(afficheur);
+        }
+
         algo.configure(generateur);
-
-        Observer<GenerateurAsync> afficheur1 = new Afficheur();
-        Observer<GenerateurAsync> afficheur2 = new Afficheur();
-        Observer<GenerateurAsync> afficheur3 = new Afficheur();
-        Observer<GenerateurAsync> afficheur4 = new Afficheur();
-
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(10);
-        Canal canal1  = new Canal(generateur, afficheur1, scheduler, 0);
-        Canal canal2  = new Canal(generateur, afficheur2, scheduler, 500);
-        Canal canal3  = new Canal(generateur, afficheur3, scheduler, 1000);
-        Canal canal4  = new Canal(generateur, afficheur4, scheduler, 2000);
-
-        generateur.attach(canal1);
-        generateur.attach(canal2);
-        generateur.attach(canal3);
-        generateur.attach(canal4);
-
-        canal1.attach(afficheur1);
-        canal2.attach(afficheur2);
-        canal3.attach(afficheur3);
-        canal4.attach(afficheur4);
 
         generateur.generateValue();
     }
