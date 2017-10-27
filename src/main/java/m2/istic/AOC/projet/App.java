558a8ae128646850799932393a98fddf04872f6f
aanne <aurelien.anne-giffard@wanadoo.fr>
2017-10-27 17:02:05 +0200

    Ajout de 4 canaux

--- src/main/java/m2/istic/AOC/projet/App.java
+++ src/main/java/m2/istic/AOC/projet/App.java
@@ -16,15 +16,33 @@ import java.util.concurrent.ScheduledExecutorService;
 public class App 
 {
     public static void main( String[] args ) {
+
         Observer<GenerateurAsync> afficheur = new Afficheur();
         AlgoDiffusion algo = new BasicDiffusion();
         Generateur generateur = new GenerateurImpl(0, algo);
         algo.configure(generateur);
-        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(10);
-        Canal canal  = new Canal(generateur, afficheur, scheduler);
 
-        generateur.attach( (ObserverAsync) canal);
-        canal.attach(afficheur);
+
+        Observer<GenerateurAsync> afficheur1 = new Afficheur();
+        Observer<GenerateurAsync> afficheur2 = new Afficheur();
+        Observer<GenerateurAsync> afficheur3 = new Afficheur();
+        Observer<GenerateurAsync> afficheur4 = new Afficheur();
+
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(10);
+        Canal canal1  = new Canal(generateur, afficheur1, scheduler);
+        Canal canal2  = new Canal(generateur, afficheur2, scheduler);
+        Canal canal3  = new Canal(generateur, afficheur3, scheduler);
+        Canal canal4  = new Canal(generateur, afficheur4, scheduler);
+
+        generateur.attach(canal1);
+        generateur.attach(canal2);
+        generateur.attach(canal3);
+        generateur.attach(canal4);
+
+        canal1.attach(afficheur1);
+        canal2.attach(afficheur2);
+        canal3.attach(afficheur3);
+        canal4.attach(afficheur4);
 
         generateur.generateValue();
     }
